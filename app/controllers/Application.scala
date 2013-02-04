2bd748188348f073b7867aa0952463b4cbe3f299
Swind <idle.swind@gmail.com>
2013-02-04 10:10:55 +0800

    fix layout bug

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,16 +8,19 @@ import models._
 
 object Application extends Controller {
   
+  val rowSize = 3
+  
   def index = Action {
     Redirect(routes.Application.jobs(1))
   }
   
   def jobs(pageNum:Int = 1) = Action {
     val dataList = OpenDataAPI.listData(9,pageNum).toList
+    
     if(dataList.size < 9)
-      Ok(views.html.index(dataList, 1))
+      Ok(views.html.index(dataList.grouped(rowSize).toList, 1))
     else
-      Ok(views.html.index(dataList, pageNum+1))
+      Ok(views.html.index(dataList.grouped(rowSize).toList, pageNum+1))
   }
   
 }
